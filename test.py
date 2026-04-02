050d66b07ccec983658d7f8c78f94cef838828b4
Davit Najaryan <dnajarya@uwo.ca>
2026-04-01 21:18:24 -0400

    refactoring

--- test.py
+++ test.py
@@ -4,43 +4,68 @@ import time
 import fastf1
 
 #main function, we return nothing
-def main() -> None:
-    
-    #use cache folder for stroing downloaded data, this will speed up future runs
-    fastf1.Cache.enable_cache("cache")
+def main(session) -> None:
 
-    #get and load the session
-    test_session = fastf1.get_session(2021, "Spanish Grand Prix", "R")
-    test_session.load()
 
     #print the session information
-    print(f"Event: {test_session.event['EventName']}")
-    print(f"Event date: {test_session.event['EventDate']}")
-    print(f"Session: {test_session.name}")
+    print(f"Event: {session.event['EventName']}")
+    print(f"Event date: {session.event['EventDate']}")
+    print(f"Session: {session.name}")
     
-    print("\nResults:\n", test_session.results.head())
+    print("\nResults:\n", session.results.head())
     
-    print("\nLaps:\n", test_session.laps.head())
+    print("\nLaps:\n", session.laps.head())
 
 
-def column_names() -> None:
+def column_names(session) -> None:
     
     #get the column names of the results and laps dataframes
-    test_session = fastf1.get_session(2021, "Spanish Grand Prix", "R")
-    test_session.load()
     
-    print("Results columns:\n", test_session.results.columns)
-    print("\nLaps columns:\n", test_session.laps.columns)
+    print("Results columns:\n", session.results.columns)
+    print("\nLaps columns:\n", session.laps.columns)
+    
+#this function will be used to extract colums from results that will help preduct winners 
+def extract_useful_colums_results_race(session) -> None:
+    
+    #driver id
+    print("\nDriver IDs:\n", session.results.get("DriverId"))
+    
+    #position final
+    print("\nPositions:\n", session.results.get("Position"))
+    
+    #grid position <-- imporatant ??
+    print("\nGrid positions:\n", session.results.get("GridPosition"))
+    
+    #points
+    print("\nPoints:\n", session.results.get("Points"))
+    
+    
+    
+#fast extraction of cache, saves time and bandwith
+def store_cache() -> None:
+    
+    #store cache in a folder called "cache"
+    fastf1.Cache.enable_cache("cache")
+    
 
 #if this file is being run directly, call the main function
 #otherwise, dont run main() 
 if __name__ == "__main__":
-    main()
+    
+    #load race
+    test_session = fastf1.get_session(2021, "Spanish Grand Prix", "R")
+    test_session.load()
+    
+    #store the file in cache
+    store_cache()
+    
+    main(test_session)
     
     #wait 5 seonds for readability
-    for i in range(6):
-        print("Waiting... " + str(i) + " seconds...")
+    for i in range(5):
+        print("Waiting... " + str(i + 1) + " seconds...")
         time.sleep(1)
     
-    column_names()
+    column_names(test_session)
     
+    extract_useful_colums_results_race(test_session)
